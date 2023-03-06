367aa31c4637d2b66c288a3be2fe91ecb69c2dcf
AJoelj84 <ajoelj84@gmail.com>
2023-03-06 15:00:59 -0500

    getting API to work changed javascript

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-fetch('https://api.openweathermap.org/data/2.5/forecast?lat={35.457370}&lon={-83.055780}&appid={b6533fadaacf8190721a4a03a425d1dd}',{
+fetch('https://api.openweathermap.org/data/2.5/forecast?lat=35.457370&lon=-83.055780&appid=263899f28c1a4fdfb9c42daf32e3c285',{
 })
 .then(function(response){
     return response.json();
