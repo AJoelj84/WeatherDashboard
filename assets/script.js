175f7e5969493d66faea6ed397beca497e11e2f1
AJoelj84 <ajoelj84@gmail.com>
2023-03-06 14:58:58 -0500

    more code for foundation of program

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,8 @@
-fetch('https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={b6533fadaacf8190721a4a03a425d1dd}')
+fetch('https://api.openweathermap.org/data/2.5/forecast?lat={35.457370}&lon={-83.055780}&appid={b6533fadaacf8190721a4a03a425d1dd}',{
+})
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    console.log(data);
+});
