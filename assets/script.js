eeba3088a1dff9eb987e6173a5645d85e29607cc
AJoelj84 <ajoelj84@gmail.com>
2023-03-07 14:30:12 -0500

    Working further on JavaScript functions

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,30 @@
-var zipCode = $("#city-enter");
-
 var fetchWeather = function(zipCode) {
-    var enteredCity = 'http://api.openweathermap.org/geo/1.0/zip?zip='+zipCode+'&appid=263899f28c1a4fdfb9c42daf32e3c285'
-        console.log(zipCode)
-    var loadWeather = 'https://api.openweathermap.org/data/2.5/forecast?' + enteredCity + '&appid=263899f28c1a4fdfb9c42daf32e3c285';
-    
-    console.log('Weather');
-    fetch(loadWeather)
-        .then (function(response){
-            console.log('Weather Retrieved');
+    var enteredCity = 'http://api.openweathermap.org/geo/1.0/zip?zip=' + zipCode + '&appid=263899f28c1a4fdfb9c42daf32e3c285';
+    console.log(zipCode);
+    fetch(enteredCity)
+        .then(function(response) {
+            console.log('City Retrieved');
             return response.json();
         })
-        .then (function(data){
-            console.log('Weather', data);
-        })
+        .then(function(data) {
+            var lat = data.lat;
+            var lon = data.lon;
+            var loadWeather = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=263899f28c1a4fdfb9c42daf32e3c285';
+            console.log('Weather');
+            fetch(loadWeather)
+                .then(function(response) {
+                    console.log('Weather Retrieved');
+                    return response.json();
+                })
+                .then(function(data) {
+                    console.log('Weather', data);
+                });
+        });
 };
 
 var submitBtn = $("#subBtn");
 submitBtn.on("click", function(event){
     event.preventDefault();
-    fetchWeather(zipCode.val())
+    var zipCode = $("#city-enter").val();
+    fetchWeather(zipCode);
 });
