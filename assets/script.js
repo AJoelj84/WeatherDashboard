60f9c709549cf174d9348323d3ed0891b4067c03
AJoelj84 <ajoelj84@gmail.com>
2023-03-07 14:04:35 -0500

    Added JQuery and Bootstrap Code, and finished function to pull weather Data

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,20 @@
-fetch('https://api.openweathermap.org/data/2.5/forecast?lat=35.457370&lon=-83.055780&appid=263899f28c1a4fdfb9c42daf32e3c285',{
-})
-.then(function(response){
-    return response.json();
-})
-.then(function(data){
-    console.log(data);
+var city = $("#city-enter");
+
+var fetchWeather = function(city) {
+    var loadWeather = 'https://api.openweathermap.org/data/2.5/weather?q='+ city + '&appid=263899f28c1a4fdfb9c42daf32e3c285';
+    console.log('Weather');
+    fetch(loadWeather)
+        .then (function(response){
+            console.log('Weather Retrieved');
+            return response.json();
+        })
+        .then (function(data){
+            console.log('Weather', data);
+        })
+};
+
+var submitBtn = $("#subBtn");
+submitBtn.on("click", function(event){
+    event.preventDefault();
+    fetchWeather(city.val())
 });
