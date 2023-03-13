a60f0a5c0b950129051e38d8fbbba35e71f4ed0e
AJoelj84 <ajoelj84@gmail.com>
2023-03-13 11:48:50 -0400

    Made Changes to API call functions and HTML

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// Variable and Functions to Fetch Lattitude and Longitude by Zip Code, then API call 5 day forecast
 var fetchWeather = function(zipCode) {
     var enteredCity = 'http://api.openweathermap.org/geo/1.0/zip?zip=' + zipCode + '&appid=263899f28c1a4fdfb9c42daf32e3c285';
     console.log(zipCode);
@@ -9,7 +10,7 @@ var fetchWeather = function(zipCode) {
         .then(function(data) {
             var lat = data.lat;
             var lon = data.lon;
-            var loadWeather = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=263899f28c1a4fdfb9c42daf32e3c285';
+            var loadWeather = 'https://api.openweathermap.org/data/2.5/forecast/?lat=' + lat + '&lon=' + lon + '&cnt=5&appid=263899f28c1a4fdfb9c42daf32e3c285&units=imperial';
             console.log('Weather');
             fetch(loadWeather)
                 .then(function(response) {
@@ -21,7 +22,7 @@ var fetchWeather = function(zipCode) {
                 });
         });
 };
-
+// Submit Button for Zip Code Input
 var submitBtn = $("#subBtn");
 submitBtn.on("click", function(event){
     event.preventDefault();
