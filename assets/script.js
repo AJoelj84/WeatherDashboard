5f80ab754dbcf6b126c40cfcef00e8dbb7cebde5
AJoelj84 <ajoelj84@gmail.com>
2023-03-13 14:49:13 -0400

    Started adding elements for local storage

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,14 @@
 // Variable and Functions to Fetch Lattitude and Longitude by Zip Code, then API call 5 day forecast
+
+
+
 var fetchWeather = function(zipCode) {
     var enteredCity = 'http://api.openweathermap.org/geo/1.0/zip?zip=' + zipCode + '&appid=263899f28c1a4fdfb9c42daf32e3c285';
     console.log(zipCode);
+
+    
+
+
     fetch(enteredCity)
         .then(function(response) {
             console.log('City Retrieved');
@@ -72,3 +79,17 @@ submitBtn.on("click", function(event){
     var zipCode = $("#city-enter").val();
     fetchWeather(zipCode);
 });
+
+
+
+$(document).ready(function() {
+    // Retrieve previous searches from local storage
+    var previousSearches = JSON.parse(localStorage.getItem("previousSearches")) || [];
+    
+    // Loop through previous searches and create Bootstrap cards
+    $.each(previousSearches, function(index, search) {
+      var card = $("<div>").addClass("card").appendTo("#previous-searches");
+      $("<div>").addClass("card-body").html(search).appendTo(card);
+    });
+  });
+
